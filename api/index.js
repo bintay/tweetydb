69235c97b002e9febc7fa566dfd170e726ba3f54
Ben Taylor <bentaylor@bentaylor.me>
2021-03-27 17:55:05 -0500

    added cors

--- api/index.js
+++ api/index.js
@@ -11,8 +11,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
    cors: {
-     origin: ["http://localhost:3000", "https://tweetydb.com/"],
-     methods: ["GET", "POST"]
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
    }
 });
 
