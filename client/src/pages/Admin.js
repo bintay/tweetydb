ccbfc30bbeb918b3b982da592a674f97fc625388
Ben Taylor <bentaylor@bentaylor.me>
2021-04-02 21:56:23 -0500

    added filtering to admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -6,6 +6,10 @@ const APIPrefix = process.env.NODE_ENV === 'production' ? 'https://tweetydb-api.
 
 const Admin = () => {
    const [ logs, setLogs ] = useState([]);
+   const [ nameFilter, setNameFilter ] = useState('');
+   const [ questionFilter, setQuestionFilter ] = useState('');
+   const [ onlyShowIncorrect, setOnlyShowIncorrect ] = useState(false);
+
    const mapLogs = (log, index) => {
       const newLog = {
          'Name': log.name,
@@ -34,7 +38,57 @@ const Admin = () => {
 
    return (
       <div>
-         <Table style={{ background: '#000' }} noMaxHeight title='Recent Submissions' rows={logs.sort((a, b) => b.logId - a.logId).map(mapLogs)} />
+         <h1>Mentor Logs</h1>
+         <div>
+            <h2 style={{ marginBottom: -10 }}>Filter By</h2>
+            <label style={{ fontSize: 20, whiteSpace: 'nowrap' }}>
+               Name: 
+               <input 
+                  style={{ fontSize: 20, padding: 2, width: 200, minWidth: 0 }} 
+                  type='text'
+                  value={nameFilter} 
+                  onChange={(e) => setNameFilter(e.target.value)} 
+               />
+            </label>
+            <span style={{ width: 40, display: 'inline-block' }} />
+            <label style={{ fontSize: 20, whiteSpace: 'nowrap' }}>
+               Question: 
+               <input 
+                  style={{ fontSize: 20, padding: 2, width: 60, minWidth: 0 }} 
+                  type='text' 
+                  value={questionFilter} 
+                  onChange={(e) => setQuestionFilter(e.target.value)} 
+               />
+            </label>
+            <span style={{ width: 40, display: 'inline-block' }} />
+            <label style={{ fontSize: 20, whiteSpace: 'nowrap' }}>
+               Only Incorrect Submission: 
+               <input 
+                  style={{ fontSize: 20, padding: 2, width: 20, minWidth: 0, verticalAlign: 'top', marginTop: 24, marginLeft: 4 }} 
+                  type='checkbox' 
+                  checked={onlyShowIncorrect} 
+                  onChange={(e) => setOnlyShowIncorrect(e.target.checked)} 
+               />
+            </label>
+         </div>
+         {onlyShowIncorrect}
+         <Table 
+            style={{ background: '#000' }} 
+            noMaxHeight 
+            title='Recent Submissions' 
+            rows={
+               logs
+               .filter((row) => (nameFilter.length === 0 || row.name.toLowerCase() === nameFilter.toLowerCase())
+                                 && (questionFilter.length === 0 || row.id.toString() === questionFilter)
+                                 && (!onlyShowIncorrect || !row.correct))
+               .sort((a, b) => b.logId - a.logId)
+               .map(mapLogs)
+            } 
+         />
+         {logs.filter((row) => !((nameFilter.length === 0 || row.name.toLowerCase() === nameFilter.toLowerCase())
+                                && (questionFilter.length === 0 || row.id.toString() === questionFilter)
+                                && (!onlyShowIncorrect || !row.correct))).length}
+         &nbsp;row(s) not shown due to filters
       </div>
    )
 };
